30e20a939193f8a9562157a86d0743c1a53df1ac
Pavel Kosach <kosachdev@gmail.com>
2023-03-10 21:51:45 +0300

    fixed counter animation

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -69,14 +69,33 @@ const calc = (price = 100) => {
     }
 
     // Анимация калькулятора
-    let arr = [];
-    for (let i = 0; i < totalValue; i++) {
-      arr.push(i);
+    function animate({ timing, draw, duration }) {
+      let start = performance.now();
+
+      requestAnimationFrame(function animate(time) {
+        // timeFraction изменяется от 0 до 1
+        let timeFraction = (time - start) / duration;
+        if (timeFraction > 1) timeFraction = 1;
+
+        // вычисление текущего состояния анимации
+        let progress = timing(timeFraction);
+
+        draw(progress); // отрисовать её
+
+        if (timeFraction < 1) {
+          requestAnimationFrame(animate);
+        }
+      });
     }
-    arr.forEach(function (element, i) {
-      setTimeout(function () {
-        total.textContent = i;
-      }, 5 * ++i);
+
+    animate({
+      duration: 110,
+      timing(timeFraction) {
+        return timeFraction;
+      },
+      draw(progress) {
+        total.textContent = Math.round(totalValue * progress);
+      },
     });
   };
 
